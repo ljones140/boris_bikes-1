d4ca825c3db0632d23ff35d9a45d81be1b3780cf
Diego Regules <diegoregules@gmail.com>
2015-07-21 17:03:47 +0100

    Docking Stations Release Bikes - Test Passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
-  it "Responds to release_bike" do
+  it 'Responds to release_bike' do
     expect(subject).to respond_to :release_bike
+  end
+  it 'releases/gets bike that works' do
+    expect(subject.release_bike).to be_a Bike
+    expect(subject.release_bike).to be_working
   end
 end
 

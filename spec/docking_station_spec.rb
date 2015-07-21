d2ed78f63d7971bc09355e9c82eaa9265e9b17f1
Diego Regules <diegoregules@gmail.com>
2015-07-21 13:00:05 +0100

    Unit test - Docking Station: release bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,15 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  it "docking station responds to Release bike" do
+    docking_station = DockingStation.new
+    docking_station.respond_to? (:release_bike)
+  end
 end
+
+
+
+
+# describe DockingStation do
+#   it {should respond_to(:release_bike)}
+# end

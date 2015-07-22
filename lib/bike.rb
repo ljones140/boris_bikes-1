4b23d4081e8d0aaceea1112a468d366da9e2d7a1
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-07-22 18:12:59 +0100

    Added ability for bikes to be broken

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,5 +1,14 @@
 class Bike
+  def initialize
+    @broken = false
+  end
   def working?
-    true
+    return true unless @broken
+  end
+  def report_broken
+    @broken = true
+  end
+  def broken?
+    @broken
   end
 end

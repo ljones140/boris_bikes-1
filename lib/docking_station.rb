c68a6c9dbb6c3dd5f7d41b437f6026fcf73727d1
lewis <ljones140@gmail.com>
2015-07-22 11:55:15 +0100

    release_bike and dock working together

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,10 @@
 require_relative 'bike'
 class DockingStation
   def release_bike
-    #Bike.new
-    raise "No bikes available"
+    raise "No bikes available" unless @bike
+    @bike
   end
   def dock(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,15 +2,18 @@ require 'docking_station'
 
 describe DockingStation do
 
-  xit 'releases/gets bike that works' do
+  it 'releases/gets bike that works' do
+    subject.dock(Bike.new)
     expect(subject.release_bike).to be_a Bike
     expect(subject.release_bike).to be_working
   end
   it "responds to dock with 1 arg" do
      expect(subject).to respond_to(:dock).with(1).argument
   end
-  it "gives error if empty of bikes" do
-    expect{subject.release_bike}.to raise_error 'No bikes available'
+  desribe '#release_bike' do
+    it "gives error if empty of bikes" do
+      expect{subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
 end
 

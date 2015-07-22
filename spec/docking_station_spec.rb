f5d2c8dbd99d636f987db658f953ed4d3b790c9f
lewis <ljones140@gmail.com>
2015-07-22 11:18:39 +0100

    unit test for dock ready

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,10 +2,15 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
   it 'releases/gets bike that works' do
     expect(subject.release_bike).to be_a Bike
     expect(subject.release_bike).to be_working
   end
+  it "responds to dock with 1 arg" do
+     expect(subject).to respond_to(:dock).with(1).argument
+   end
+
 end
+
+
 
